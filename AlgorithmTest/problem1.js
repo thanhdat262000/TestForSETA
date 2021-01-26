c8f2ab21c41132cc050681738b971bfa3ec760af
Nguyễn Thành đạt <thanhdat262000@gmail.com>
2021-01-26 22:45:25 +0700

    add-algoritmh-problems

---
+++ AlgorithmTest/problem1.js
@@ -0,0 +1,38 @@
+// Provide an array of strings, eg: [‘a’, ‘ab’, ‘abc’, ‘cd’, ‘def, ‘gh’].
+// Write a function to find the strings’ length that appear most in this array.
+// Writing the unit test function and provide some test-cases.
+// The result for example array is [‘ab’, ‘cd’, ‘gh’]
+function maxStringLength(array) {
+  const stringLength = array.map((string) => string.length);
+  stringLength.sort((a, b) => a - b);
+  console.log(stringLength);
+  let count = 1;
+  let maxCountlength;
+  let maxCount = 0;
+  for (let i = 0; i < stringLength.length; i++) {
+    if (stringLength[i] === stringLength[i + 1]) count += 1;
+    else {
+      if (count > maxCount) {
+        maxCount = count;
+        maxCountlength = stringLength[i];
+      }
+      count = 1;
+    }
+  }
+  console.log(maxCountlength);
+  return array.filter((string) => string.length === maxCountlength);
+}
+console.log(
+  maxStringLength([
+    "a",
+    "ab",
+    "abc",
+    "cd",
+    "def",
+    "gbd",
+    "bsd",
+    "asd",
+    "sdf",
+    "g",
+  ])
+);
